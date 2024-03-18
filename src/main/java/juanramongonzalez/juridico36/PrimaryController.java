2e2f603180b69704e9f8c343e6cda92b42ef0636
juanr <juanr@PCJUANRAMON>
2024-03-18 17:09:37 -0600

    Primer Commit

---
+++ src/main/java/juanramongonzalez/juridico36/PrimaryController.java
@@ -0,0 +1,12 @@
+package juanramongonzalez.juridico36;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
