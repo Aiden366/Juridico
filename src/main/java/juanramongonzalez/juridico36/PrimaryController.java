d2ff53a85f8c9fd92ded817a2f16146c20b2e142
juanr <juanr@PCJUANRAMON>
2024-04-07 20:25:40 -0600

    Carga 2

--- src/main/java/juanramongonzalez/juridico36/PrimaryController.java
+++ src/main/java/juanramongonzalez/juridico36/PrimaryController.java
@@ -2,8 +2,12 @@ package juanramongonzalez.juridico36;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class PrimaryController {
+    @FXML
+    private TextField nombre;
+    
 
     @FXML
     private void switchToSecondary() throws IOException {
